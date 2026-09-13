7794553610ad4239bcf301012d23741e03d20eed
Helos-dev <scazzo340@gmail.com>
2026-09-13 15:31:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import os
 
-import os
-
 BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
 
